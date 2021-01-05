123cc3973857c9ef7cf549c79a3d1c02d9f912e6
PinJui <71209752+PinJui@users.noreply.github.com>
2021-01-05 15:42:18 +0800

    Add files via upload

---
+++ code/VDSR/demo.py
@@ -0,0 +1,80 @@
+import argparse
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+import cv2
+import os
+import glob
+
+import vdsr
+
+# Source: https://stackoverflow.com/questions/29100722/equivalent-im2double-function-in-opencv-python
+def im2double(image):
+    info = np.iinfo(image.dtype) # Get the data type of the input image
+    return image.astype(np.float32) / info.max # Divide all values by the largest possible value in the datatype
+
+def bicubic_sr(input, scale):
+    bicubic_output = np.clip(cv2.resize(input, None, fx=1.0 * scale, fy=1.0 * scale, interpolation=cv2.INTER_CUBIC), 0, 255).astype(np.uint8)
+
+    return bicubic_output
+
+def VDSR_sr(sess, VDSR, input, scale):
+    upscaled_rgb = np.clip(cv2.resize(input, None, fx=1.0 * scale, fy=1.0 * scale, interpolation=cv2.INTER_CUBIC), 0, 255).astype(np.uint8)
+
+    upscaled_rgb = im2double(upscaled_rgb.astype(np.uint8))
+
+    VDSR_output = sess.run(VDSR.output, feed_dict={VDSR.X:np.expand_dims(upscaled_rgb, axis=0)})
+    VDSR_output = np.squeeze(VDSR_output, axis=0)
+    VDSR_output *= 255
+
+    return VDSR_output
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_channel', type=int, default=3, help='-')
+    parser.add_argument('--scale', type=int, default=3, help='-')
+    args, unknown = parser.parse_known_args()
+
+    scale = args.scale
+
+    result_save_path = './demo_result'
+    if not os.path.exists(result_save_path): os.makedirs(result_save_path)
+
+    print("Scale {}x !!".format(scale))
+    print("Please enter the path of the image's directory. Ex) '/data/test_images' ")
+    path = input("Enter the directory path: ")
+    print("Please enter the name of images. This version 'only' read '.png', 'jpg' extention and 'RGB' image! Ex) '0.png'")
+    print("If you want to read all images in the directory, enter '*.png' or '*.jpg'.")
+    name = input("Enter the image name: ")
+
+    image_path = path + '/' + name
+    images_list = glob.glob(image_path)
+
+    if not images_list:
+        print("Path is empty. Check that the directory or image name is correct.")
+    else:
+        VDSR = vdsr.VDSR(args)
+        VDSR.neuralnet()
+
+        sess = tf.Session()
+        saver = tf.train.Saver()
+        saver.restore(sess, './model/VDSR.ckpt')
+
+        for image in images_list:
+            filename = os.path.basename(image)[:-4]
+
+            label = cv2.imread(image).astype(np.uint8)
+            #height, width =  label.shape[:2]
+
+            if len(label.shape) != 3:
+                print("{}.png is not RGB 3 channel image".format(filename))
+                break
+            #elif height*width > 750*750:
+            #    print("{}.png' size is large. Recommend a size smaller than 750x750.".format(filename))
+            else:
+                VDSR_upscale = VDSR_sr(sess, VDSR, label.copy(), scale=scale)
+                bicubic_upscale = bicubic_sr(label.copy(), scale=scale)
+
+                cv2.imwrite('{}/{}.png'.format(result_save_path, '{}_bicubic_{}x'.format(filename, scale)), bicubic_upscale)
+                cv2.imwrite('{}/{}.png'.format(result_save_path, '{}_VDSR_{}x'.format(filename, scale)), VDSR_upscale)
---
+++ code/VDSR/train.py
@@ -0,0 +1,125 @@
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import os
+import time
+import numpy as np
+
+from VDSR.util import ImageBatch
+from VDSR.util import psnr
+from VDSR.util import Time
+
+def training(VDSR, config):
+    training_ratio = 1
+    main_data_path = '.'
+    default_test_scale = 3
+    '''
+    DATA SET PATH
+    '''
+    train_x_images_path = '{}/data/train_291_input/*.npy'.format(main_data_path)
+    train_y_images_path = '{}/data/train_291_label/*.npy'.format(main_data_path)
+    test_labels_path = '{}/data/customSet5/ground_truth/*.npy'.format(main_data_path)
+    test_inputs_path = '{}/data/customSet5/blur_{}x/*.npy'.format(main_data_path, default_test_scale)
+    '''
+    TRAIN SET(291) and shuffle
+    '''
+    train_x_images_batch = ImageBatch(train_x_images_path, training_ratio=1, on_sort=True, ext='npy')
+    train_y_images_batch = ImageBatch(train_y_images_path, training_ratio=1, on_sort=True, ext='npy')
+
+    shuffle_indicese = list(range(train_x_images_batch.N_TRAIN_DATA))
+    np.random.shuffle(shuffle_indicese)
+    train_x_images_batch.train_shuffle(shuffle_indicese)
+    train_y_images_batch.train_shuffle(shuffle_indicese)
+    '''
+    TEST SET(SET 5) & preprocessing
+    '''
+    test_labels_batch = ImageBatch(test_labels_path, training_ratio=training_ratio, on_sort=True, ext='npy')
+    test_inputs_batch = ImageBatch(test_inputs_path, training_ratio=training_ratio, on_sort=True, ext='npy')
+    test_labels = test_labels_batch.next_batch(batch_size=5)
+    test_inputs = test_inputs_batch.next_batch(batch_size=5)
+
+    avg_bicubic_psnr_rgb = 0
+    for i in range(len(test_labels)):
+        _psnr = psnr(test_labels[i], test_inputs[i], peak=1)
+        avg_bicubic_psnr_rgb += _psnr/5
+
+    '''
+    HYPERPARAMETER
+    '''
+    training_epoch = config.training_epoch
+    batch_size = config.batch_size
+    n_data = train_x_images_batch.N_TRAIN_DATA
+    total_batch = n_data // batch_size if n_data%batch_size ==0 else (n_data//batch_size) +1
+    total_iteration = training_epoch * total_batch
+
+    # total_global_step = epoch*total_batch = n_iteration
+    learning_rate = tf.train.exponential_decay(learning_rate=config.learning_rate, global_step=VDSR.global_step,
+                                               decay_steps=80 * total_batch, decay_rate=0.1, staircase=True)
+
+    VDSR.neuralnet()
+    VDSR.optimize(learning_rate=learning_rate, grad_clip=config.grad_clip, on_grad_clipping=config.on_grad_clipping)
+    VDSR.summary(learning_rate=learning_rate)
+
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.per_process_gpu_memory_fraction = 0.8
+    sess = tf.Session(config=tf_config)
+    writer = tf.summary.FileWriter('./model/vdsr_result', sess.graph)
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+
+    print("Total the number of Data : " + str(train_x_images_batch.N_TRAIN_DATA))
+    print("Total Step per 1 Epoch: {}".format(total_batch))
+    print("The number of Iteration: {}".format(total_iteration))
+
+    for epoch in range(training_epoch):
+        avg_cost = 0
+        avg_vdsr_psnr_rgb = 0
+        for i in range(total_batch):
+            start = time.time()
+
+            batch_x = train_x_images_batch.next_batch(batch_size, num_thread=8, astype='array')
+            batch_y = train_y_images_batch.next_batch(batch_size, num_thread=8, astype='array')
+
+            summaries, _cost, _, g_step, lr = sess.run(
+                [VDSR.summaries, VDSR.cost, VDSR.optimizer, VDSR.global_step, learning_rate],
+                feed_dict={VDSR.X: batch_x, VDSR.Y: batch_y})
+
+            writer.add_summary(summaries, g_step)
+            avg_cost += _cost / total_batch if np.isinf(_cost) != True else 0
+
+            end = time.time()
+            if i == 5:
+                Time.require_time(start, end, count=total_iteration - g_step)
+
+        '''
+        Evaluate VDSR performance
+       '''
+        for index in range(len(test_labels)):
+            '''
+            RGB test average PSNR
+           '''
+            label = (test_labels[index].copy()*255).astype(np.uint8)
+            input = test_inputs[index].copy()
+
+            input_y = np.expand_dims(input, axis=0)
+            result = sess.run(VDSR.output, feed_dict={VDSR.X: input_y})[0]
+            result = (result*255).astype(np.uint8)
+
+            _psnr = psnr(label, result, peak=255)
+            print(_psnr)
+            avg_vdsr_psnr_rgb += _psnr / 5
+
+        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost), 'Learning Rate: ', lr,
+              '\nRGB AVG PSNR:: Bicubic: {:.9f} || VDSR: {:.9f}'.format(avg_bicubic_psnr_rgb, avg_vdsr_psnr_rgb))
+
+        '''
+        Do shuffle train_set for 1 epoch
+        '''
+        np.random.shuffle(shuffle_indicese)
+        train_x_images_batch.train_shuffle(shuffle_indicese)
+        train_y_images_batch.train_shuffle(shuffle_indicese)
+
+    print("학습 완료!")
+    save_path = '{}/model/VDSR.ckpt'.format(os.path.abspath('.'))
+    saver.save(sess, save_path)
+    print("세이브 완료")
